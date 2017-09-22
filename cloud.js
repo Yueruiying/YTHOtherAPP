92e273e9134a5389606e7fe54a729b16c948244d
yueruiying <1334993200@qq.com>
2017-09-22 19:03:49 +0800

    手机号改为接收人手机号码

--- cloud.js
+++ cloud.js
@@ -4,7 +4,7 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request) {
-	sendBusnissFeedback('15158898469', {phoneNum: '15158898469', userName: '悦瑞英', company: '典丰科技', city: '杭州'}).then(function (ok) {
+	sendBusnissFeedback('13777867678', {phoneNum: '15158898469', userName: '悦瑞英', company: '典丰科技', city: '杭州'}).then(function (ok) {
 	  console.log(ok)
 	}, function (err) {
 	  console.error(err)
